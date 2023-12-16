9f2a453d02bfb89ec59d1b709907c63338fe32d3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-16 20:29:30 +0100

    Portfolio: Reordening items

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,11 @@
+// Portfolio.js
+
 import React, { useEffect } from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "../styles/portfolio.css";
-import { Link } from "react-router-dom";
 import portfolioData from "../data/portfolioData.json";
-import { AiOutlineCaretLeft, AiOutlineCaretRight } from "react-icons/ai";
 import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer"; // Import useInView
+import { useInView } from "react-intersection-observer";
 
 const Portfolio = () => {
   const [ref, inView] = useInView({
@@ -21,75 +20,50 @@ const Portfolio = () => {
     }
   }, [inView, controls]);
 
-  const carouselSettings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "150px",
-    slidesToShow: 3,
-    speed: 500,
-    prevArrow: (
-      <motion.div
-        className="slick-custom-arrow"
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.5 }}
-      >
-        <AiOutlineCaretLeft />
-      </motion.div>
-    ),
-    nextArrow: (
-      <motion.div
-        className="slick-custom-arrow"
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.5 }}
-      >
-        <AiOutlineCaretRight />
-      </motion.div>
-    ),
-  };
-
-  // Define animation variants for the Portfolio section
   const portfolioVariants = {
     hidden: { opacity: 0, x: -100 },
     visible: {
       opacity: 1,
       x: 0,
       transition: {
-        duration: 1, // Adjust the animation duration
-        ease: "easeInOut", // Adjust the easing function
+        duration: 1,
+        ease: "easeInOut",
       },
     },
   };
 
   return (
     <motion.div
-      className="carousel-container"
+      className="portfolio-container"
       ref={ref}
       initial="hidden"
       animate={controls}
       variants={portfolioVariants}
     >
-      <h2>Portfolio</h2>
       <div className="slider-wrapper">
-        <Slider {...carouselSettings}>
-          {portfolioData.map((item) => (
-            <div className="carousel-item" key={item.id}>
-              <Link to={`/portfolio/${item.id}`} className="port-items">
-                <img
-                  src={require(`../assets/Portfolio/${item.image}`)}
-                  alt={`Item ${item.id}`}
-                />
+        {portfolioData.map((item, index) => (
+          <div
+            className={`portfolio-item ${index % 2 === 0 ? "even" : "odd"}`}
+            key={item.id}
+          >
+            <div className="portfolio-item-content">
+              <img
+                src={require(`../assets/Portfolio/${item.image}`)}
+                alt={`Item ${item.id}`}
+              />
+              <div className="portfolio-text">
                 <h3>{item.itemTitle}</h3>
+                <p>{item.description}</p>
                 <ul>
                   {item.technologies &&
                     item.technologies.map((tech, index) => (
                       <li key={index}>{tech}</li>
                     ))}
                 </ul>
-              </Link>
+              </div>
             </div>
-          ))}
-        </Slider>
+          </div>
+        ))}
       </div>
     </motion.div>
   );
