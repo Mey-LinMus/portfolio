ffc1e49935ccfb0f7eaf95f9b33a178adc2fd4b5
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-12-17 18:26:57 +0100

    Header: adding logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typewriter from "typewriter-effect/dist/core";
 import "../styles/header.css";
+import Logo from "../assets/MLogo.svg";
 
 const Header = () => {
+  const typewriterRef = useRef(null);
+  useEffect(() => {
+    const typewriter = new Typewriter(typewriterRef.current, {
+      loop: false,
+      delay: 60,
+      cursor: "|",
+    });
+
+    typewriter
+      .typeString("Hey, Ik ben <strong> Mey-Lin Mus.</strong>")
+      .pauseFor(1000)
+      .typeString(
+        " Ik ben een gedreven student aan de Erasmushogeschool Brussel, waar ik de opleiding Multimedia & Creatieve Technologie volg. Ik ben gepassioneerd door de voortdurend veranderende geconnecteerde wereld.\n" +
+          " Als enthousiast persoon sta ik altijd open voor het leren van nieuwe dingen. Als doorzetter geef ik niet snel op.\n" +
+          "Ik werk nauwkeurig en met het nodige geduld. Bovendien werk ik graag in teamverband en ben ik goed in communiceren."
+      )
+      .start();
+  }, []);
+
   return (
-    <div className="v1_2">
-      <a href="" className="contact">
-        Contact
-      </a>
-      <div className="v4_3"></div>
-      <span className="v4_5">
-        Ik ben een gedreven student aan de Erasmushogeschool Brussel, waar ik de
-        opleiding Multimedia & Creatieve Technologie volg. Ik ben gepassioneerd
-        door de voortdurend veranderende geconnecteerde wereld. Als enthousiast
-        persoon sta ik altijd open voor het leren van nieuwe dingen. Als
-        doorzetter geef ik niet snel op. Ik werk nauwkeurig en met het nodige
-        geduld. Bovendien werk ik graag in teamverband en ben ik goed in
-        communiceren.
-      </span>
-      <span className="v4_6">Hey, Ik ben Mey-Lin Mus!</span>
-      <div className="v6_13"></div>
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div id="hearder-text">
+        <span ref={typewriterRef} className="header-info"></span>
+      </div>
+      <div className="profile-picture"></div>
     </div>
   );
 };
