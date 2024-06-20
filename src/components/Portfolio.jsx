0c3b77a4b6d04130d6f222c1a4a2d882ade4441e
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-20 23:35:48 +0200

    Adding data

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,84 +1,90 @@
-// Portfolio.js
 import React, { useEffect } from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "../styles/portfolio.css";
+import { Link } from "react-router-dom";
 import portfolioData from "../data/portfolioData.json";
+import { AiOutlineCaretLeft, AiOutlineCaretRight } from "react-icons/ai";
 import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import { useInView } from "react-intersection-observer"; // Import useInView
 
 const Portfolio = () => {
   const [ref, inView] = useInView({
     triggerOnce: true,
   });
+
   const controls = useAnimation();
+
   useEffect(() => {
     if (inView) {
       controls.start("visible");
     }
   }, [inView, controls]);
 
+  const carouselSettings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "150px",
+    slidesToShow: 3,
+    speed: 500,
+    prevArrow: (
+      <motion.div
+        className="slick-custom-arrow"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.5 }}
+      >
+        <AiOutlineCaretLeft />
+      </motion.div>
+    ),
+    nextArrow: (
+      <motion.div
+        className="slick-custom-arrow"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.5 }}
+      >
+        <AiOutlineCaretRight />
+      </motion.div>
+    ),
+  };
+
+  // Define animation variants for the Portfolio section
   const portfolioVariants = {
     hidden: { opacity: 0, x: -100 },
     visible: {
       opacity: 1,
       x: 0,
       transition: {
-        duration: 1,
-        ease: "easeInOut",
+        duration: 1, // Adjust the animation duration
+        ease: "easeInOut", // Adjust the easing function
       },
     },
   };
 
   return (
-    <motion.div className="portfolio-container">
+    <motion.div
+      className="carousel-container"
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={portfolioVariants}
+    >
+      <h2>Portfolio</h2>
       <div className="slider-wrapper">
-        {portfolioData.map((item, index) => (
-          <div
-            className={`portfolio-item ${index % 2 === 0 ? "even" : "odd"}`}
-            key={item.id}
-          >
-            <section
-              className="portfolio-item-content"
-              ref={ref}
-              initial="hidden"
-              animate={controls}
-              variants={portfolioVariants}
-            >
-              <h2>{item.itemTitle}</h2>
-              <div className="image-wrapper">
+        <Slider {...carouselSettings}>
+          {portfolioData.map((item) => (
+            <div className="carousel-item" key={item.id}>
+              <Link to={`/portfolio/${item.id}`} className="port-items">
                 <img
                   src={require(`../assets/Portfolio/${item.image}`)}
                   alt={`Item ${item.id}`}
                 />
-                <div className="hover-info">
-                  <div className="hover-text">
-                    <p>{item.description}</p>
-                    <ul>
-                      {item.technologies && (
-                        <li key={index}>{item.technologies.join(" | ")}</li>
-                      )}
-                    </ul>
-                  </div>
-                  <motion.button
-                    className="website-button"
-                    whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
-                  >
-                    {item.file && (
-                      <a
-                        href={item.file}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="link-button"
-                      >
-                        Visit Website
-                      </a>
-                    )}
-                  </motion.button>
-                </div>
-              </div>
-            </section>
-          </div>
-        ))}
+                <h3>{item.itemTitle}</h3>
+                <p>{item.course}</p>
+              </Link>
+            </div>
+          ))}
+        </Slider>
       </div>
     </motion.div>
   );
