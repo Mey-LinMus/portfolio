1c5550f2d91352f954a88b9fd48e71103a338b9c
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-09-30 16:14:34 +0200

    Header: editing text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
       .typeString("Hey, Ik ben <strong> Mey-Lin Mus.</strong>")
       .pauseFor(1000)
       .typeString(
-        " Ik heb gestudeerd aan de Erasmushogeschool Brussel, waar ik de opleiding Multimedia & Creatieve Technologie heb gevolgd. Ik ben gepassioneerd door de voortdurend veranderende geconnecteerde wereld.\n" +
+        " Ik heb gestudeerd aan de Erasmushogeschool Brussel, waar ik de opleiding Multimedia & Creatieve Technologie heb gevolgd.\n" +
           "Als enthousiast persoon sta ik altijd open voor het leren van nieuwe dingen. Als doorzetter geef ik niet snel op.\n" +
           "Ik werk nauwkeurig en met het nodige geduld. Bovendien werk ik graag in teamverband en ben ik goed in communiceren."
       )
