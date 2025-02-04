a7dfdaf524acef5710a0c6188f27fdcba29dd957
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2025-02-04 22:46:49 +0100

    Fixing download

--- src/components/DownloadCVButton.jsx
+++ src/components/DownloadCVButton.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import '../styles/DownloadCVButton.css';
+import React from "react";
+import "../styles/DownloadCVButton.css";
 
 const DownloadCVButton = () => {
-
   const handleDownloadCV = () => {
-    const link = document.createElement('a');
-    link.href = '../assets/CV-Mey-Lin_Mus.pdf'; 
-    link.download = 'CV-Mey-Lin_Mus.pdf'; 
+    const link = document.createElement("a");
+    link.href = "/CV-Mey-Lin_Mus.pdf";
+    link.download = "CV-Mey-Lin_Mus.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
